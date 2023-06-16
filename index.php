371d06fde111fd94399f8f12f2c3ccef64c533c5
antonionicolaci <antonionicolaci48@gmail.com>
2023-06-16 14:20:22 +0200

    add form's input

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include __DIR__ . "/functions.php";
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,13 +10,55 @@ include __DIR__ . "/functions.php";
     <title>Password Generator</title>
 </head>
 <body>
+    <?php 
+    if ($lPass != '') {?>
+        <h1>La password generata è <?php passCreator($arrSign, $lPass, $repeat, $opt);
+    } else { ?>
+    
     <form action="" method="get">
-        <input type="text">
-        <button>invia</button>
+		<label for="lPass">
+			Inserisci la lunghezza della password:
+		</label>
+        <input type="number" id="lPass" name="lPass">
+        <h3>Ripetere i caratteri?</h3>
+				<div>
+					<input type="radio" name="rep" id="radio-si" value='1'>
+					<label for="radio-si">
+						Si, ripeti.
+					</label>
+				</div>
+				<div>
+					<input type="radio" name="rep" id="radio-no" value='0'>
+					<label for="radio-no">
+						No, non ripetere.
+					</label>
+				</div>
+
+
+				<h3>Seleziona caratteri:</h3>
+				<div>
+					<input type="checkbox" id="lettere" name="opt[]" value='0'>
+					<label for="lettere">
+						Lettere.
+					</label>
+				</div>
+				<div>
+					<input type="checkbox" id="numeri" name="opt[]" value='1'>
+					<label for="numeri">
+						Numeri.
+					</label>
+				</div>
+				<div>
+					<input type="checkbox" id="simboli" name="opt[]" value='2'>
+					<label for="simboli">
+						Simboli.
+					</label>
+				</div>
+        <button type="submit">invia</button>
         <button type="reset">reset</button>
     </form>
-    <?php
         
-    ?>
+    <?php
+    } ?>
 </body>
 </html>
